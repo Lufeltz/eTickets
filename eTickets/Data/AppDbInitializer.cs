444b250caa9d6aed4aaa7812a48019dc953161b4
Lufeltz <lufelt@proton.me>
2023-12-04 23:48:41 -0300

    feat: Criar configuração inicial do AppDbInitializer

---
+++ eTickets/Data/AppDbInitializer.cs
@@ -0,0 +1,44 @@
+﻿namespace eTickets.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                context.Database.EnsureCreated();
+
+                //Cinema
+                if (!context.Cinemas.Any())
+                {
+
+                }
+                //Actors
+                if (!context.Actors.Any())
+                {
+
+                }
+                //Producers
+                if (!context.Producers.Any())
+                {
+
+                }
+                //Movies
+                if (!context.Movies.Any())
+                {
+
+                }
+                //Actors & Movies
+                if (!context.Actors_Movies.Any())
+                {
+
+                }
+
+
+            }
+        }
+    }
+}
