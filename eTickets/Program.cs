c47fb94f9674571ada1bbf9691403dea80d9aebd
Lufeltz <lufelt@proton.me>
2023-12-12 19:44:50 -0300

    feat: Adicionar registro do serviço IActorsService

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,4 +1,6 @@
 using eTickets.Data;
+using eTickets.Data.Services;
+using eTickets.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 // DbContext configuration
 //builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnectionString"]));
+
+// Services
+builder.Services.AddScoped<IActorsService, ActorsService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
