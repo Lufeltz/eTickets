ecc5c24df3fe1a5fcaab37f54fc5eba20b0a0dcb
Lufeltz <lufelt@proton.me>
2023-12-11 23:36:32 -0300

    feat: Criar uma interface para o ActorsController
    Essa interface será responsável por armazenar as assinaturas dos métodos implementados em ActorsService

---
+++ eTickets/Data/Services/IActorsService.cs
@@ -0,0 +1,13 @@
+﻿using eTickets.Models;
+
+namespace eTickets.Data.Services
+{
+    public interface IActorsService
+    {
+        Task<IEnumerable <Actor>> GetAll();
+        Actor GetActor(int id);
+        void AddActor(Actor actor);
+        Actor UpdateActor(int id, Actor newActor);
+        void DeleteActor(int id);
+    }
+}
